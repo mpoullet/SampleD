bd4fa15d05242c425371da96bef065a25238bf27
Matthieu Poullet <m.poullet@avm.de>
2012-02-08 15:47:54 +0100

    It now runs.

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all:
+	test -d build || mkdir -p build
 	gcc -Wall -o build/SampleD src/SampleD.c
 	gcc -Wall -o build/report  src/report.c
 
---
+++ run.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+set -x
+echo "Generate a Launch Daemon called SampleD in build/"
+make clean all
+echo "Copy the daemon in a root-owned, non-user directory: /tmp/SampleD"
+sudo cp build/SampleD /tmp
+echo "Load the plist (Property List) associated with the daemon"
+sudo launchctl load src/com.apple.dts.SampleD.plist
+echo "Now let's try to talk with the socket specified in the plist: 12345"
+./src/client.pl
+echo "Unload the plist"
+sudo launchctl unload src/com.apple.dts.SampleD.plist
