70ac3da14caadcd6d325a5a2b17ce20f65e62368
Matthieu Poullet <m.poullet@avm.de>
2012-02-09 16:38:45 +0100

    Build with XCode

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #/bin/sh
-set -x
+#set -x
 echo "Generate a Launch Daemon called SampleD in build/"
 ./build_mac.sh
 echo "Copy the daemon in a root-owned, non-user directory: /tmp/SampleD"
-sudo cp build/SampleD/Debug/SampleD /tmp
+sudo cp build/Debug/SampleD /tmp
 echo "Load the plist (Property List) associated with the daemon"
 sudo launchctl load src/com.apple.dts.SampleD.plist
 echo "Now let's try to talk with the socket specified in the plist: 12345"
 ./src/client.pl
 echo "Unload the plist"
 sudo launchctl unload src/com.apple.dts.SampleD.plist
-echo "syslog -d /private/var/log/asl"
+echo "syslog -d /private/var/log/asl | grep SampleD"
